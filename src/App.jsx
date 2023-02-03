66a57315b0a2429b0b81e14db5eee0e6a0ade0e0
LarsG <lars.ga.solheim@gmail.com>
2023-02-03 09:59:59 +0100

    added simple genarting of lists of JSX

--- src/App.jsx
+++ src/App.jsx
@@ -7,35 +7,27 @@ import { DiJavascript } from 'react-icons/di'
 // Components
 import BioCard from './components/BioCard/BioCard'
 
+import data from './data/developers'
+
+function PersonCard(props) {
+  return (
+    <li>
+      <h4>{props.name}</h4>
+      <a href="www.example.com">Example</a>
+    </li>
+  )
+}
+
 function App() {
   return (
     <div className="App">
 
-      <BioCard
-        imgSrc="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
-        name="Lars Gunnar"
-        short="Some fancy descript short text"
-      >
-
-        {/* Skill Icons here */}
-        <DiJavascript size={30} />
-        <DiJavascript size={30} />
-
-      </BioCard>
-
-      <BioCard
-        imgSrc={profileSampleMale}
-        name="Lars Gunnar"
-        short="Some fancy descript short text"
-      >
-
-        {/* Skill Icons here */}
-        <DiJavascript size={30} />
-        <DiJavascript size={30} />
-
-      </BioCard>
-
-      <div className='placeholder'></div>
+      <ul>
+        {
+          data.map(
+            (name, index) => <PersonCard key={index} name={name}/>)
+        }
+      </ul>
 
     </div>
   )
---
+++ src/data/developers.js
@@ -0,0 +1,11 @@
+const data = [
+  "Lars G",
+  "Deepti",
+  "Mats",
+  "Remi",
+  "Artemijs",
+  "Stefan",
+  "Inger Johanne"
+]
+
+export default data
